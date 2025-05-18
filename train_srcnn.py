6ffd65b559f07824997bacc59fb3e075d7f308f0
xplsek03 <michal.plsek@seznam.cz>
2025-05-18 12:55:36 +0200

    residual 1

--- train_srcnn.py
+++ train_srcnn.py
@@ -10,7 +10,7 @@ import ssim
 
 from torch import nn
 from torchinfo import summary
-from layers import SqrtLoss, ConvLayer
+from models.layers import SqrtLoss, ConvLayer
 from models.baseline_models import SRCNN, VGG_Loss, freeze_model, unfreeze_model, SRResNet
 from dataset import ImageDataset
 from train import train, compare_images
